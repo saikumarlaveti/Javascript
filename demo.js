8221b899ab976dd8aaef9600342da749fb55d796
saikumar <saikumarlaveti5061@gmail.com>
2026-03-01 20:08:56 +0530

    functions

---
+++ demo.js
@@ -0,0 +1,9 @@
+const student = {
+    Name : "Saikumar Laveti",
+    RollNo : "22MCA40",
+    Grade : "A",
+    Age : 26
+}
+console.log(student) { Name: 'Saikumar Laveti', RollNo: '22MCA40', Grade: 'A', Age: 26 }
+delete student.Age
+console.log(student) { Name: 'Saikumar Laveti', RollNo: '22MCA40', Grade: 'A' }
---
+++ functions.js
@@ -0,0 +1,23 @@
+
+//1.function without parameter
+function greet(){
+    console.log('hello world')
+}
+
+greet();
+
+//2.function with parameter
+
+function wish(name){
+    console.log('My name is ' + name)
+}
+
+wish('Saikumar Laveti')
+
+//3.function with return type
+
+function addition (a,b){
+    return a+b;
+}
+c = addition(5,4)
+console.log(c)
